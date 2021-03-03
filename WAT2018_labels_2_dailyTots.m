e228a131a01ed1ddada2dfd21d82758540976d84
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2021-03-02 17:41:03 -0800

    Make it easier to change prob/RL/# clicks thresholds

--- WAT2018_labels_2_dailyTots.m
+++ WAT2018_labels_2_dailyTots.m
@@ -7,10 +7,14 @@
 
 
 clearvars
-inDir = 'G:\New_Atl_CTs\Labeled_Clicks\NFC_HAT_WATLabels\HAT_A_06\ClusterToClassify';
-baseDir = 'I:\forNNet\WAT_2018_trainingExamples';
-saveName = 'HAT_A_06_DailyTotals'; % filename to save 
-saveDir = 'G:\New_Atl_CTs\DailyCT_Totals\OtherDeps';
+inDir = 'J:\WAT_BC_01\NEW_ClusterBins_120dB\ToClassify'; % directory containing ToClassify files
+baseDir = 'G:\cluster_NNet\Set_w_Combos_HighAmp';
+saveName = 'WAT_BC_01_DailyTotals'; % filename to save 
+saveDir = 'G:\DailyCT_Totals';
+RLThresh = 120;
+numClicksThresh = 0;
+probThresh = 0;
+
 labelDir = fullfile(inDir,'labels');
 fList = dir(fullfile(inDir,'*.mat'));
 
@@ -21,18 +25,25 @@ spNameList = [{typeList(:).name}';'UO']; % species names corresponding to NNet l
 
 % Compile labeled bins across the deployment; rows correspond to spNameList; 
 % columns to cluster_bins files; each cell contains the start/end times of 
-% all bins given that label in that file which meet the min prob and min 
+% all bins given that label in that file which meet the min PPRL and min 
 % nspec thresholds
 labeledBins = {}; 
 for iF = 1:size(fList,1)
     load(fullfile(fList(iF).folder,fList(iF).name))
     load(fullfile(labelDir,strrep(fList(iF).name,'toClassify.mat','predLab.mat')))
+     load(fullfile(labCertDir,strrep(labCertFiles(i).name,'clusters_PR95_PPmin120_toClassify.mat','TPWS1')),'MTT','MPP');
+    load(fullfile(labCertDir,strrep(labCertFiles(i).name,'clusters_PR95_PPmin120_toClassify.mat','ID1')));
+    binTimes = (floor(MTT(1)):datenum([0,0,0,0,5,0]):ceil(MTT(end)))';
+    
+    % calculate 
+    
     probs = double(probs);
     predLabels = double(predLabels)+1;
-    probIdx = sub2ind(size(probs),[1:size(probs,1)],double(predLabels));
+    probIdx = sub2ind(size(probs),1:size(probs,1),double(predLabels));
     myProbs = probs(probIdx);
-    predLabels(myProbs<.99) = NaN;
-    predLabels(nSpecMat<50)=NaN;
+    predLabels(myProbs<probThresh) = NaN;
+    predLabels(meanPPRL<RLThresh) = NaN;
+    predLabels(nSpecMat<numClicksThresh)=NaN;
     for iS = 1:19
         labeledBins{iS,iF} = sumTimeMat(predLabels==iS,:);
     end
