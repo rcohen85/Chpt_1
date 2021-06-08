c0f6ca373d7c21b34edb4292e50b29d07b750177
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2021-06-08 15:44:37 -0700

    Calculate mean spectra & percentiles

--- characterizeCTs.m
+++ characterizeCTs.m
@@ -1,14 +1,14 @@
 %% Calculate summary stats for click types used to train a neural net; plot
 % mean spectra, ICI distributions, mean waveform envelopes
 
-% clearvars
-% 
-% % directory containing folders with composite_clusters output training examples
-% trainDir = 'I:\cluster_NNet\TrainSetSummary\MostRepresentativeClusters';
-% saveDir = 'I:\cluster_NNet\TrainSetSummary';
-% TPWSDirs = {'J:\','H:\HAT_B_01-03'};
-% samps = 3e3; %WARNING: make sure all classes have at least this many clicks 
-% % available or indexing will get messed up later on
+clearvars
+
+% directory containing folders with composite_clusters output training examples
+trainDir = 'I:\cluster_NNet\TrainSetSummary\MostRepresentativeClusters';
+saveDir = 'I:\cluster_NNet\TrainSetSummary';
+TPWSDirs = {'J:\JAX10C'};
+samps = 3e3; %WARNING: make sure all classes have at least this many clicks 
+% available or indexing will get messed up later on
 
 %% Figure out how many clicks contributed to each class and which deployments 
 % they came from; then pick random sample from each class
@@ -85,100 +85,125 @@
 % 
 % compiledClicks = cell(size(ClickSummary,1),6);
 % compiledClicks(1:size(ClickSummary,1),1) = ClickSummary(1:size(ClickSummary,1),1);
-
-% open TPWS files one at a time and grab appropriate clicks for each class
-for i = 371:size(uniqueTPWSfiles,1)
-    
-    % get filepath
-    findTPWS = strcmp({fullFileList.name},uniqueTPWSfiles{i});
-    
-    % load TPWS vars
-    if sum(findTPWS)==0 || sum(findTPWS)>1
-        fprintf('Warning: %s not found or found in multiple locations, skipping to next file\n',uniqueTPWSfiles{i});
-    elseif sum(findTPWS)==1
-        
-        fprintf('Pulling clicks from file %d of %d\n',i,size(uniqueTPWSfiles,1));
-        tic
-        load(fullfile(fullFileList(findTPWS).folder,fullFileList(findTPWS).name));
-        toc
-        % find which clicks need to be saved for which classes
-        findClicks = find(strcmp(allTPWSfiles,uniqueTPWSfiles{i})); %indices of all clicks from this TPWS
-        classEdges = 1:samps:size(allTPWSfiles,1)+samps;
-        classEdges(end) = classEdges(end)-1;
-        [N,~,class] = histcounts(findClicks,classEdges); %bin these indices by class      
-        C = unique(class);
-        
-        % save clicks to appropriate classes
-        for k = 1:size(C)
-            
-            clicksThisClass = find(class==C(k));
-            timesThisClass = allClickTimes(findClicks(clicksThisClass));
-            [~,indsThisClass,ib] = intersect(MTT,timesThisClass);
-            
-            if size(timesThisClass,1)==size(indsThisClass,1)
-                compiledClicks{C(k),2} = [compiledClicks{C(k),2};MTT(indsThisClass)];
-                compiledClicks{C(k),3} = [compiledClicks{C(k),3};MSP(indsThisClass,:)];
-                compiledClicks{C(k),4} = [compiledClicks{C(k),4};MSN(indsThisClass,:)];
-                compiledClicks{C(k),5} = [compiledClicks{C(k),5};MPP(indsThisClass)];
-                compiledClicks{C(k),6} = [compiledClicks{C(k),6};allTPWSfiles(findClicks(clicksThisClass))];
-            else
-                fprintf('  Didn''t locate the correct number of clicks for class %d\n',C(k));
-                compiledClicks{C(k),2} = [compiledClicks{C(k),2};MTT(indsThisClass)];
-                compiledClicks{C(k),3} = [compiledClicks{C(k),3};MSP(indsThisClass,:)];
-                compiledClicks{C(k),4} = [compiledClicks{C(k),4};MSN(indsThisClass,:)];
-                compiledClicks{C(k),5} = [compiledClicks{C(k),5};MPP(indsThisClass)];
-                compiledClicks{C(k),6} = [compiledClicks{C(k),6};allTPWSfiles(findClicks(clicksThisClass(ib)))];
-            end
-        end
-    end
-    
-    if mod(i,10)==0
-       save(fullfile(saveDir,'CompiledClicks'),'compiledClicks','-v7.3');
-    end
-end
+% 
+% % open TPWS files one at a time and grab appropriate clicks for each class
+% for i = 192:209;%1:size(uniqueTPWSfiles,1)
+%     
+%     % get filepath
+%     findTPWS = strcmp({fullFileList.name},uniqueTPWSfiles{i});
+%     
+%     % load TPWS vars
+%     if sum(findTPWS)==0 || sum(findTPWS)>1
+%         fprintf('Warning: %s not found or found in multiple locations, skipping to next file\n',uniqueTPWSfiles{i});
+%     elseif sum(findTPWS)==1
+%         
+%         fprintf('Pulling clicks from file %d of %d\n',i,size(uniqueTPWSfiles,1));
+%         tic
+%         load(fullfile(fullFileList(findTPWS).folder,fullFileList(findTPWS).name));
+%         toc
+%         % find which clicks need to be saved for which classes
+%         findClicks = find(strcmp(allTPWSfiles,uniqueTPWSfiles{i})); %indices of all clicks from this TPWS
+%         classEdges = 1:samps:size(allTPWSfiles,1)+samps;
+%         classEdges(end) = classEdges(end)-1;
+%         [N,~,class] = histcounts(findClicks,classEdges); %bin these indices by class      
+%         C = unique(class);
+%         
+%         % save clicks to appropriate classes
+%         for k = 1:size(C)
+%             
+%             clicksThisClass = find(class==C(k));
+%             timesThisClass = allClickTimes(findClicks(clicksThisClass));
+%             [~,indsThisClass,ib] = intersect(MTT,timesThisClass);
+%             
+%             if size(timesThisClass,1)==size(indsThisClass,1)
+%                 compiledClicks{C(k),2} = [compiledClicks{C(k),2};MTT(indsThisClass)];
+%                 compiledClicks{C(k),3} = [compiledClicks{C(k),3};MSP(indsThisClass,:)];
+%                 compiledClicks{C(k),4} = [compiledClicks{C(k),4};MSN(indsThisClass,:)];
+%                 compiledClicks{C(k),5} = [compiledClicks{C(k),5};MPP(indsThisClass)];
+%                 compiledClicks{C(k),6} = [compiledClicks{C(k),6};allTPWSfiles(findClicks(clicksThisClass))];
+%             else
+%                 fprintf('  Didn''t locate the correct number of clicks for class %d\n',C(k));
+%                 compiledClicks{C(k),2} = [compiledClicks{C(k),2};MTT(indsThisClass)];
+%                 compiledClicks{C(k),3} = [compiledClicks{C(k),3};MSP(indsThisClass,:)];
+%                 compiledClicks{C(k),4} = [compiledClicks{C(k),4};MSN(indsThisClass,:)];
+%                 compiledClicks{C(k),5} = [compiledClicks{C(k),5};MPP(indsThisClass)];
+%                 compiledClicks{C(k),6} = [compiledClicks{C(k),6};allTPWSfiles(findClicks(clicksThisClass(ib)))];
+%             end
+%         end
+%     end
+%     
+%     if mod(i,10)==0
+%        save(fullfile(saveDir,'CompiledClicks'),'compiledClicks','-v7.3');
+%     end
+% end
 
 %% Compute summary statistics for each CT
 summaryStats = struct('CT',[],'MeanSpecs',[],'ICIdists',[],'PeakFreq',[],'BW_3dB',[]);
 
-%             if size(thisType.Tfinal{1,1},2) == 188
-%                 data.BinSpecs = [data.BinSpecs;thisType.Tfinal{1,1}];
-%                 
-%             elseif size(thisType.Tfinal{1,1},2) == 191
-%                 data.BinSpecs = [data.BinSpecs;thisType.Tfinal{1,1}(2:189)];
-%             end
-%             data.ICI = [data.ICI;thisType.Tfinal{1,2}];
-%             data.Deployment = [data.Deployment;depVec];
+for i = [1:10,12:size(compiledClicks,1)]
+    
+%     % linearize, normalize, average
+%     linSpecs = 10.^(compiledClicks{i,3}./20);
+%     linSpecs = linSpecs-min(linSpecs,[],2);
+%     linSpecs_norm = linSpecs./max(linSpecs,[],2);
+%     sumSpec = 20*log10(mean(linSpecs_norm,1));
+%     specPerctle_linNorm = 20*log10(prctile(linSpecs_norm,[25,75]));
+%     
+%     figure
+%     hold on
+%     plot(4.5:0.5:99.5,sumSpec,'k','LineWidth',2)
+%     plot(4.5:0.5:99.5,specPerctle_linNorm(1,:),'--k','LineWidth',2)
+%     plot(4.5:0.5:99.5,specPerctle_linNorm(2,:),'--k','LineWidth',2)
+%     hold off
+%     title('Linearize, Normalize, Average')
+    
+    % Compute mean spectrum & percentiles (normalize, linearize, average)
+    specs = compiledClicks{i,3}-min(compiledClicks{i,3},[],2);
+    specs_norm = specs./max(specs,[],2);
+    specPerctle_norm = prctile(specs_norm,[25,75]);
+    specsNorm_lin = 10.^(specs_norm./20);
+    sumSpec = 20*log10(mean(specsNorm_lin,1));
+    
+    
+    figure
+    hold on
+    plot(4.5:0.5:99.5,sumSpec,'k','LineWidth',2)
+    plot(4.5:0.5:99.5,specPerctle_norm(1,:),'--k','LineWidth',2)
+    plot(4.5:0.5:99.5,specPerctle_norm(2,:),'--k','LineWidth',2)
+    hold off
+%     title('Normalize, Linearize, Average');
+    
+    
+        
+    %      ICI = sum(data.ICI,1);
+    %
+    %      pf = f(find(sumSpec_norm == max(sumSpec_norm)));
+    %
+    %      summaryStats(b).CT = trainSet(k).name;
+    %      summaryStats(b).MeanSpecs = sumSpec_norm;
+    %      summaryStats(b).ICIdists = ICI;
+    %      summaryStats(b).PeakFreq = pf;
+    
+end
 
-%      sumSpec = mean(data.BinSpecs,1);
-%      sumSpec = sumSpec-min(sumSpec);
-%      sumSpec_norm = sumSpec./max(sumSpec);
-%      
-%      ICI = sum(data.ICI,1);
-%      
-%      pf = f(find(sumSpec_norm == max(sumSpec_norm)));
-%      
-%      summaryStats(b).CT = trainSet(k).name;
-%      summaryStats(b).MeanSpecs = sumSpec_norm;
-%      summaryStats(b).ICIdists = ICI;
-%      summaryStats(b).PeakFreq = pf;
 %% Plot
 % f = 5:0.5:98.5;
 
 % q = size(summaryStats,2);
 % m = ceil(q/n); % number of columns of subplots
 
-for i = 1:size(summaryStats,2)
-    
-    figure(1)
-    subplot(1,2,1)
-    plot(f,summaryStats(i).MeanSpecs)
-    xlim([5,100])
-    ylim([0 1])
-    xlabel('Frequency (kHz)');
-    ylabel('Normalized Amplitude');
-    
-    subplot(1,2,2)
-   
-    
-end
+% for i = 1:size(summaryStats,2)
+%     
+%     figure(1)
+%     subplot(1,2,1)
+%     plot(f,summaryStats(i).MeanSpecs)
+%     xlim([5,100])
+%     ylim([0 1])
+%     xlabel('Frequency (kHz)');
+%     ylabel('Normalized Amplitude');
+%     
+%     subplot(1,2,2)
+%    
+%     
+% end
 
