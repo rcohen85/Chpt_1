ea779ef646e1c135486f2f456333f98b49265721
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2021-09-16 10:38:17 -0700

    combine separate Atl Me and GoM Me classes to add a new "All Me" class to DailyCT_Totals files

---
+++ CombineGervais.m
@@ -0,0 +1,16 @@
+fileList = dir(fullfile('I:\DailyCT_Totals','*.mat'));
+
+for i = 1:size(fileList,1)
+    
+   load(fullfile(fileList(i).folder,fileList(i).name));
+   spNameList{21,1} = 'AtlGervais+GomGervais';
+   binFeatures{1,21} = [binFeatures{1,10};binFeatures{1,11}];
+   binFeatures{2,21} = [binFeatures{2,10};binFeatures{2,11}];
+   binFeatures{3,21} = [binFeatures{3,10};binFeatures{3,11}];
+   dailyTots(:,22) = dailyTots(:,11)+dailyTots(:,12);
+   labeledBins{1,21} = [labeledBins{1,10};labeledBins{1,11}];
+   
+   save(fullfile(fileList(i).folder,fileList(i).name), 'spNameList',...
+       'RLThresh','numClicksThresh','probThresh','labeledBins','binFeatures','dailyTots','-v7.3');
+    
+end
